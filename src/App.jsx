5fa7486d6c0753bef524c0a308e69ffd0e8ff01d
SheepTester <ussa.say@gmail.com>
2020-07-25 12:45:07 -0700

    Use jsx and standard lol

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from './logo.svg'
+import styles from './App.css'
+
+function App () {
+  return (
+    <div className={styles.App}>
+      thing
+    </div>
+  )
+}
+
+export default App
